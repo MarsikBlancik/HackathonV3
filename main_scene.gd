a93425e6bc09de9b9da2f708bd448ea8ab7b22de
GardiPL <dawid.gardecki@edu.uekat.pl>
2026-04-17 22:29:34 +0200

    healthbar, energybar
    sss

--- main_scene.gd
+++ main_scene.gd
@@ -1,16 +1,26 @@
 extends Node2D
 
-# Pobieramy referencje do węzłów w naszej scenie
-# Upewnij się, że nazwy w cudzysłowach idealnie pasują do nazw węzłów w edytorze
+@onready var player = $player
+@onready var ui = $UI
 
+func _ready() -> void:
+	print("--- MAIN SCENE START ---")
+	if player == null:
+		print("BŁĄD: Nie mogę znaleźć węzła Player! (Sprawdź nazwę)")
+	if ui == null:
+		print("BŁĄD: Nie mogę znaleźć węzła UI! (Sprawdź nazwę)")
+		
+	if player and ui:
+		print("SUKCES: Znalazłem Gracza i UI. Łączę sygnały...")
+		player.hp_changed.connect(ui.update_hp)
+		player.energy_changed.connect(ui.update_energy)
+		
+		# Pierwsza aktualizacja na starcie
+		ui.update_hp(player.current_hp, player.max_hp)
+		ui.update_energy(player.current_energy, player.max_energy)
 
-
-
-# Funkcja wywoływana, gdy gracz umrze
 func _on_player_died() -> void:
 	get_tree().change_scene_to_file("res://Restart.tscn")
 
-# Funkcja wywoływana, gdy gracz kliknie przycisk
 func _on_restart_button_pressed() -> void:
-	# Przeładowuje całkowicie obecną scenę (resetuje grę)
 	get_tree().reload_current_scene()
---
+++ ui.gd
@@ -0,0 +1,46 @@
+extends CanvasLayer
+
+@onready var hp_bar = $HPBar
+@onready var energy_bar = $EnergyBar
+
+# Pobieramy referencje do naszych nowych tekstów (upewnij się, że nazwy się zgadzają!)
+@onready var hp_label = $HPBar/HPLabel
+@onready var energy_label = $EnergyBar/EnergyLabel
+
+func _ready() -> void:
+	# Czekamy jedną klatkę, żeby upewnić się, że Player zdążył załadować się do gry
+	await get_tree().process_frame
+	
+	# Szukamy jedynego węzła w grupie "Player"
+	var player = get_tree().get_first_node_in_group("Player")
+	
+	if player:
+		# Podpinamy sygnały BEZPOŚREDNIO pod gracza
+		player.hp_changed.connect(update_hp)
+		player.energy_changed.connect(update_energy)
+		
+		# Wymuszamy pierwszą aktualizację pasków
+		update_hp(player.current_hp, player.max_hp)
+		update_energy(player.current_energy, player.max_energy)
+		print("UI: Pomyślnie podłączono do Gracza!")
+	else:
+		print("BŁĄD: UI nie znalazło Gracza. Upewnij się, że masz gracza na scenie!")
+
+# Funkcje odbierające sygnał
+func update_hp(current: int, max_val: int) -> void:
+	if hp_bar:
+		hp_bar.max_value = max_val
+		hp_bar.value = current
+	
+	# Aktualizujemy tekst na pasku
+	if hp_label:
+		hp_label.text = str(current) + " / " + str(max_val)
+
+func update_energy(current: int, max_val: int) -> void:
+	if energy_bar:
+		energy_bar.max_value = max_val
+		energy_bar.value = current
+		
+	# Aktualizujemy tekst na pasku (np. "3 / 3")
+	if energy_label:
+		energy_label.text = str(current) + " / " + str(max_val)
