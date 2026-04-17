a3718de256bb258f7442223dd8b39ded91c75336
Mikołaj Kokot <panmeek@proton.me>
2026-04-17 20:32:37 +0200

    Basic main menu impl

---
+++ main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _ready() -> void:
+	var play = $VBoxContainer/Play
+	var settings = $VBoxContainer/Settings
+	var quit = $VBoxContainer/Quit
+	
+	play.pressed.connect(_play_pressed)
+	settings.pressed.connect(_settings_pressed)
+	quit.pressed.connect(_quit_pressed)
+	
+
+func _play_pressed():
+	print("play")
+	
+func _settings_pressed():
+	print("settings")
+	
+func _quit_pressed():
+	get_tree().quit()
